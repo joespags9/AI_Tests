e542fb146c7cc3e54be1e1ed8a5812201d9643de
joespags9 <105925795+joespags9@users.noreply.github.com>
2022-12-14 08:38:21 -0500

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ async function startServer() {
         graphiql: true
     }))
 
-    await mongoose.connect(<MONGO_INFO>)
+    await mongoose.connect('mongodb+srv://joespags9:Ben04Riley06@cluster0.kvtzb.mongodb.net/Sentences_Two?retryWrites=true&w=majority')
 
     app.listen(PORT, () => {
         console.log("Server running")
